139717ba91441fa5a875d93af93d248fc84425ec
lpseccp <lpseccp@gmail.com>
2025-04-29 17:17:51 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,27 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('escuro-cache').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './style.css',
-        './app.js',
-        './joystick.js',
-        './manifest.json',
-        './icon-192x192.png',
-        './icon-512x512.png'
-      ]);
+const CACHE_NAME = "corredor-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/joystick.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
